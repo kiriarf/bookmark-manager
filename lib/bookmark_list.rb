193638cde775468b238059d6a06eed9bf6f9b249
Kiril <kiril@olgas-air.mynet>
2020-10-20 12:37:05 +0100

    test for BookmarkList #add and #show and passing code

--- lib/bookmark_list.rb
+++ lib/bookmark_list.rb
@@ -4,5 +4,15 @@ class BookmarkList
   def initialize
     @list = []
   end
+
+  def add(bookmark)
+    list << bookmark
+  end
   
+  def show
+    list.each do |bookmark|
+      puts bookmark
+    end
+  end
+
 end
--- spec/units/bookmark_list_spec.rb
+++ spec/units/bookmark_list_spec.rb
@@ -6,4 +6,19 @@ describe BookmarkList do
       expect(subject.list).to be_empty
     end
   end
+
+  describe "#add" do
+    it "can add a bookmark to the list array" do
+      subject.add("wikipedia")
+      expect(subject.list).to include("wikipedia")
+    end
+  end
+
+  describe "#show" do
+    it "can show a list of bookmarks" do
+      subject.add("wikipedia")
+      subject.add("google")
+      expect { subject.show }.to output("wikipedia\ngoogle\n").to_stdout
+    end
+  end
 end
