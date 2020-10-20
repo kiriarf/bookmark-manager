cb439cef10fec2c338b88e1ff99fff6b79696bed
Kiril <kiril@olgas-air.mynet>
2020-10-20 10:31:22 +0100

    sinatra, rspec and capybara setup

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra/base'
+
+class BookmarkManager < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    # erb(:index)
+  end
+
+  # post '/name' do
+  #   session[:name] = params[:name]
+  #   redirect '/play'
+  # end
+
+  # get '/play' do
+  #   @name = session[:name]
+  #   erb(:play)
+  # end
+
+  # post '/result' do
+  #   @choice = params[:choice]
+  #   game = Game.new(@choice)
+  #   @result = game.play
+  #   erb(:result)
+  # end
+
+  run! if app_file == $0
+end
