abb73790762074056d6487cdccc6a6155aa2a814
Maggie Hibberd <maggiehibberd@gmail.com>
2020-10-20 16:41:58 +0100

    URL's added and changes made

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,2 +1,9 @@
 -- in migrations/01_create_bookmarks_table.sql
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');
+INSERT INTO bookmarks (url) VALUES ('http://askjeeves.com');
+INSERT INTO bookmarks (url) VALUES ('http://twitter.com');
+INSERT INTO bookmarks (url) VALUES ('http://www.google.com');
+SELECT * FROM bookmarks;
+DELETE FROM bookmarks WHERE url = 'http://twitter.com';
+
